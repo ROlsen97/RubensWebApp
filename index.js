fcc364df8dba83041df542457b954538586842a7
ROlsen97 <100783426+ROlsen97@users.noreply.github.com>
2024-01-10 17:34:13 +0100

    TryingToShowPropertiesForArrayObjects

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 Vue.createApp({
     data() {
      return {
-        message: "Hejsa"
-     }
-    }
-   }).mount("#app")
+      showmore:"placeholder",
+      Items:[
+         {name: "Om mig", info: "123"},
+         {name: "Erhvervserfaring", info: "456"},
+         {name: "Sociale medier", info: "789"},
+         {name: "Github", info: "101"},
+         {name: "Kontakt", info: "112"}
+      ]
+     };
+    },
+    methods: {
+  }
+   }).mount("#app");
