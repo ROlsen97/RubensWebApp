1e97941d0c14252f131d082beb2c034dd025c29c
ROlsen97 <100783426+ROlsen97@users.noreply.github.com>
2024-01-12 13:14:38 +0100

    centered all items

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ Vue.createApp({
      return {
       selectedItem: null,
       Items:[
-         {id: 0, name: "Om mig", info:"Jeg er en datamatiker-studerende på Zealand, Sjællands erhvervsakademi, og jeg er interesseret i at lære mere om it-sikkerhed...", 
+         {id: 0, name: "Om denne side", info:"Jeg er en datamatiker-studerende på Zealand, Sjællands erhvervsakademi, og jeg er interesseret i at lære mere om it-sikkerhed...", 
          infoToShow: "", showInfo:false},
-         {id: 1, name: "Erhvervserfaring", info: "456", infoToShow: "", showInfo:false},
-         {id: 2, name: "Sociale medier", info: "789", infoToShow: "", showInfo:false},
-         {id: 3, name: "Github", info: "101", infoToShow: "", showInfo:false},
-         {id: 4, name: "Kontakt", info: "112", infoToShow: "", showInfo:false}
+         {id: 1, name: "Github", info: "101", infoToShow: "", showInfo:false},
+         {id: 2, name: "Kontakt", info: "112", infoToShow: "", showInfo:false}
       ]
      };
     },
