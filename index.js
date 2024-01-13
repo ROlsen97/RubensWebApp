82d1006aa82768b0a477b1749c6c30199e7e62ab
ROlsen97 <100783426+ROlsen97@users.noreply.github.com>
2024-01-13 09:04:55 +0100

    centeret buttons and made them responsive

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ Vue.createApp({
      return {
       selectedItem: null,
       Items:[
-         {id: 0, name: "Om denne side", info:"Jeg er en datamatiker-studerende på Zealand, Sjællands erhvervsakademi, og jeg er interesseret i at lære mere om it-sikkerhed...", 
+         {id: 0, name: "Om denne side", info:"Denne side er mit online cv, jeg har selv designet/kodet det hele", 
          infoToShow: "", showInfo:false},
          {id: 1, name: "Github", info: "101", infoToShow: "", showInfo:false},
          {id: 2, name: "Kontakt", info: "112", infoToShow: "", showInfo:false}
