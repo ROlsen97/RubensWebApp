66a81d1e5dd7cb9cc8698febab0cbc1f15584d54
ROlsen97 <100783426+ROlsen97@users.noreply.github.com>
2024-01-13 10:32:19 +0100

    changed from a to button

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ Vue.createApp({
      return {
       selectedItem: null,
       Items:[
-         {id: 0, name: "Om denne side", info:"Denne side er mit online cv. Jeg har selv designet/kodet det hele", 
+         {id: 0, name: "Om denne side", info:"Denne side er mit online cv. Den består af HTML, CSS, Vue.js, og Bootstrap", 
          infoToShow: "", showInfo:false},
          {id: 1, name: "Denne knap fører til min Github", link: "https://github.com/ROlsen97", infoToShow: "", showInfo:false},
          {id: 2, name: "Kontakt", info: "ruben2010@live.dk", infoToShow: "", showInfo:false}
@@ -16,10 +16,11 @@ Vue.createApp({
             x.infoToShow = ""; 
             x.showInfo == false
          });
-         item.showInfo = true;
-         item.infoToShow = item.info;
          if (!item.showInfo){
-            item.infoToShow="";
+            item.showInfo = true;
+            item.infoToShow = item.info;
+            } else {
+               item.showInfo = false;
             }
          },
          followlink(item){
