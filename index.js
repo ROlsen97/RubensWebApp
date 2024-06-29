44c92b1705c4e56e48fdbb4d1620be370c1fdfd8
ROlsen97 <100783426+ROlsen97@users.noreply.github.com>
2024-06-29 18:51:15 +0200

    updated cv to portfolio

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ Vue.createApp({
      return {
       selectedItem: null,
       Items:[
-         {id: 0, name: "Om denne side", info:"Denne side er mit online cv. Den består af HTML, CSS, Vue.js, og Bootstrap", 
+         {id: 0, name: "Om denne side", info:"Denne side er mit online portfolio. Den består af HTML, CSS, Vue.js, og Bootstrap", 
          infoToShow: "", showInfo:false},
          {id: 1, name: "Denne knap fører til min Github", link: "https://github.com/ROlsen97", infoToShow: "", showInfo:false},
          {id: 2, name: "Kontakt", info: "ruben2010@live.dk", infoToShow: "", showInfo:false}
