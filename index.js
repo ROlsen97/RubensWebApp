fa25740dee7036ac4ad4c90baaf7ee16fb74c41d
ROlsen97 <100783426+ROlsen97@users.noreply.github.com>
2024-06-29 18:33:40 +0200

    Updated website to SPA

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+document.addEventListener('DOMContentLoaded', function() {
+   const homeSection = document.getElementById('home');
+   const aboutMeSection = document.getElementById('aboutMe');
+   const navHome = document.getElementById('nav-home');
+   const navAboutMe = document.getElementById('nav-aboutMe');
+
+   function showSection(section){
+
+      homeSection.classList.remove('show');
+      aboutMeSection.classList.remove('show');
+      section.classList.add('show');
+
+      homeSection.style.display = 'none';
+      aboutMeSection.style.display = 'none';
+
+      section.style.display = 'block';
+
+      if (section === homeSection) {
+         navHome.classList.add('active');
+         navAboutMe.classList.remove('active');
+       } else if (section === aboutMeSection) {
+         navHome.classList.remove('active');
+         navAboutMe.classList.add('active');
+       }
+   }
+
+   navHome.addEventListener('click', function(){
+      showSection(homeSection);
+   });
+
+   navAboutMe.addEventListener('click', function(){
+      showSection(aboutMeSection);
+   });
+
+   if (window.location.hash === '#home' || !window.location.hash){
+      showSection(homeSection);
+   } else {
+      showSection(aboutMeSection);
+   }
+
+});
+
 Vue.createApp({
     data() {
      return {
